ffea7b296aca8d8dcfb879d75f0c0a4d17ecf197
Sasha Cooper <jinksy@gmail.com>
2014-05-13 17:45:31 +0100

    refactored methods

--- directory.rb
+++ directory.rb
@@ -1,36 +1,48 @@
+@students = []
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer(@students)
+end
+
+def process(selection)
+	case selection
+	when "1"
+		students = input_students
+	when "2"
+		show_students
+	when "9"
+		exit
+	else 
+		puts "I don't know what you meant, try again"
+	end
+end
+
 def interactive_menu
-	students = []
+	@students = []
 	loop do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-		selection = gets.chomp
-		case selection
-		when "1"
-			students = input_students
-		when "2"
-			print_header
-			print(students)
-			print_footer(students)
-		when "9"
-			exit
-		else 
-			puts "I don't know what you meant, try again"
-		end
+		print_menu
+		process(gets.chomp)
 	end
 end
 
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
-	students = []
 	name = gets.chomp
 	while !name.empty? do
-		students << {name: name, cohort: :May}
-		puts "Now we have #{students.length} students"
+		@students << {name: name, cohort: :May}
+		puts "Now we have #{@students.length} students"
 		name = gets.chomp
 	end
-	students
+	@students
 end
 
 def print_header
@@ -38,8 +50,8 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-	students.each do |student|
+def print_students_list
+	@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
@@ -49,3 +61,4 @@ def print_footer(names)
 end
 
 interactive_menu
+
