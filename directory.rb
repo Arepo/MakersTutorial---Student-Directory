e8105865f0c4a8a640345ce6ce12af3789bafe7c
Sasha Cooper <jinksy@gmail.com>
2014-05-15 13:47:20 +0100

    Updated directory.rb and .gitignore

--- directory.rb
+++ directory.rb
@@ -1,4 +1,24 @@
 @students = []
+require 'date'
+
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		csv_line = "#{student[:name]}, #{student[:cohort]}"
+		file.puts csv_line
+	end
+	file.close
+end
+
+# def save_students
+# 	file = File.open("students.csv", "w")
+# 	@students.each do |student|
+# 		student_data = [student[:name], student[:cohort]]
+# 		csv_line = student_data.join(",")
+# 		file.puts csv_line
+# 	end
+# 	file.close
+# end
 
 def print_menu
 	puts "1. Input the students"
@@ -19,6 +39,7 @@ def process(selection)
 	when "2"
 		show_students
 	when "9"
+		save_students
 		exit
 	else 
 		puts "I don't know what you meant, try again"
@@ -35,10 +56,18 @@ end
 
 def input_students
 	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts "To finish, just hit enter on the student's name"
 	name = gets.chomp
 	while !name.empty? do
-		@students << {name: name, cohort: :May}
+		puts "Now enter their cohort"
+		cohort = gets.chomp
+		while !(Date::MONTHNAMES).include? cohort.capitalize do
+			puts "Did you get the month right? Please reenter:"
+			cohort = gets.chomp
+		end
+		cohort = :May if cohort == ""
+		cohort = cohort.to_sym
+		@students << {name: name, cohort: cohort}
 		puts "Now we have #{@students.length} students"
 		name = gets.chomp
 	end
@@ -51,8 +80,10 @@ def print_header
 end
 
 def print_students_list
-	@students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	@students.each_with_index do |student, index|
+		if student[:name].length < 12
+			puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+		end
 	end
 end
 
@@ -61,4 +92,3 @@ def print_footer(names)
 end
 
 interactive_menu
-
